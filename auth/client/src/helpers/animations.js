48b8f6cda9202deb61457c2e29a9e1be94d2a725
qroux <quentin.roux@hotmail.fr>
2021-06-04 18:33:28 +0200

    cubic-bezier fixed

--- auth/client/src/helpers/animations.js
+++ auth/client/src/helpers/animations.js
@@ -7,11 +7,12 @@ export const animations = {
     visible: {
       x: 0,
       opacity: 1,
-      transition: { duration: 0.2 },
+      transition: { ease: 'easeOut', duration: 0.5 },
     },
     exit: {
+      x: -20,
       opacity: 0,
-      transition: { duration: 0.4 },
+      transition: { duration: 0.3 },
     },
   },
 };
